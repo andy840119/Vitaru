9b40d4d92e54b8719b6943e46e79380c411584dc
Shawdooow <banhammer1915@gmail.com>
2017-03-14 13:00:14 -0400

    Merge pieces with Hitbox / Bullet

--- osu.Game.Modes.Vitaru/Objects/Hitbox.cs
+++ osu.Game.Modes.Vitaru/Objects/Hitbox.cs
@@ -1,7 +1,9 @@
 ﻿using osu.Framework.Graphics.Containers;
-using osu.Game.Modes.Vitaru.Objects.Drawables;
 using osu.Framework.Graphics;
 using OpenTK.Graphics;
+using OpenTK;
+using osu.Game.Graphics;
+using osu.Framework.Graphics.Sprites;
 
 namespace osu.Game.Modes.Vitaru.Objects
 {
@@ -24,6 +26,58 @@ namespace osu.Game.Modes.Vitaru.Objects
                 },
             };
             Hide();
+        }
+    }
+
+
+    class HitboxPiece : Container
+    {
+        private CircularContainer hitboxContainer;
+        private object hitbox;
+
+        public HitboxPiece(Hitbox hitbox)
+        {
+            this.hitbox = hitbox;
+            Children = new Drawable[]
+            {
+                new Container
+                {
+                    Masking = true,
+                    AutoSizeAxes = Axes.Both,
+                    Origin = Anchor.Centre,
+                    Anchor = Anchor.Centre,
+                    BorderThickness = 3,
+                    Depth = 1,
+                    BorderColour = hitbox.HitboxColor,
+                    Alpha = 1f,
+                    CornerRadius = hitbox.HitboxWidth / 2,
+                    Children = new[]
+                    {
+                        new Box
+                        {
+                            Colour = Color4.White,
+                            Alpha = 1,
+                            Width = hitbox.HitboxWidth,
+                            Height = hitbox.HitboxWidth,
+                        },
+                    },
+                },
+                hitboxContainer = new CircularContainer
+                {
+                        Origin = Anchor.Centre,
+                        Anchor = Anchor.Centre,
+                        RelativeSizeAxes = Axes.Both,
+                        Scale = new Vector2(hitbox.HitboxWidth),
+                        Depth = 2,
+                        Masking = true,
+                        EdgeEffect = new EdgeEffect
+                        {
+                            Type = EdgeEffectType.Shadow,
+                            Colour = (hitbox.HitboxColor).Opacity(0.4f),
+                            Radius = 2f,
+                        }
+                }
+            };
         }
     }
 }
--- osu.Game.Modes.Vitaru/Objects/Projectiles/Bullet.cs
+++ osu.Game.Modes.Vitaru/Objects/Projectiles/Bullet.cs
@@ -1,8 +1,10 @@
-﻿using osu.Game.Modes.Vitaru.Objects.Drawables;
-using osu.Framework.Graphics;
+﻿using osu.Framework.Graphics;
 using OpenTK;
 using System;
 using OpenTK.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
 
 namespace osu.Game.Modes.Vitaru.Objects.Projectiles
 {
@@ -67,6 +69,57 @@ namespace osu.Game.Modes.Vitaru.Objects.Projectiles
         {
             bulletsLoaded--;
             Dispose();
+        }
+    }
+
+
+    class BulletPiece : Container
+    {
+        private CircularContainer bulletContainer;
+        private object bullet;
+        public BulletPiece(Bullet bullet)
+        {
+            this.bullet = bullet;
+            Children = new Drawable[]
+            {
+                new Container
+                {
+                    Masking = true,
+                    AutoSizeAxes = Axes.Both,
+                    Origin = Anchor.Centre,
+                    Anchor = Anchor.Centre,
+                    BorderThickness = 3,
+                    Depth = 1,
+                    BorderColour = bullet.BulletColor,
+                    Alpha = 1f,
+                    CornerRadius = bullet.BulletWidth / 2,
+                    Children = new[]
+                    {
+                        new Box
+                        {
+                            Colour = Color4.White,
+                            Alpha = 1,
+                            Width = bullet.BulletWidth,
+                            Height = bullet.BulletWidth,
+                        },
+                    },
+                },
+                bulletContainer = new CircularContainer
+                {
+                        Origin = Anchor.Centre,
+                        Anchor = Anchor.Centre,
+                        RelativeSizeAxes = Axes.Both,
+                        Scale = new Vector2(bullet.BulletWidth),
+                        Depth = 2,
+                        Masking = true,
+                        EdgeEffect = new EdgeEffect
+                        {
+                            Type = EdgeEffectType.Shadow,
+                            Colour = (bullet.BulletColor).Opacity(0.75f),
+                            Radius = 2f,
+                        }
+                }
+            };
         }
     }
 }
